95c515560c0e7a789c4513f7d6cbac66d55b53fa
reeceawalsh <reeceawalsh@hotmail.co.uk>
2023-06-28 14:09:11 +0100

    Added pase path for github pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-
+    basePath: "/github-pages",
     async redirects() {
         return [
             // {
