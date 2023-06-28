7f22546aa782e1f1637e404ee4687ad9a387a1ab
reeceawalsh <reeceawalsh@hotmail.co.uk>
2023-06-28 14:17:19 +0100

    updated engine ranges

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-    basePath: "/github-pages",
     async redirects() {
         return [
             // {
